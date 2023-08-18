64b2ecf1994d87a94b552357a5779cfdee732c06
nay-lek <amnuay.lek@gmai.com>
2023-08-18 09:18:29 +0700

    Add function

--- conn/conn.php
+++ conn/conn.php
@@ -1,9 +1,9 @@
 <?php
 
 	// config for his  
-	define("HOSTNAME", "192.168.1.200"); 
-	define("USDBNAME", "smra"); 
-	define("PSDBNAME", "smra11029");
+	define("HOSTNAME", "localhost"); 
+	define("USDBNAME", "sa"); 
+	define("PSDBNAME", "sa");
 	define("_db_", "hos");
 
 	$Conn = mysql_connect(HOSTNAME,USDBNAME,PSDBNAME) or die(mysql_error());
